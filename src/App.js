ac44c1cd6f237b55cc455c12e9eee5323f1c4459
vinicius <vinapro91@gmail.com>
2021-10-08 14:43:38 -0300

    fix: removed lines

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import "./App.css";
 import ProductList from "./components/product-list";
 import Provider from "./providers";
-import { CartProvider } from "./providers/cart";
-import { CatalogueProvider } from "./providers/catalogue";
 
 function App() {
   return (
     <div className="App">
-      {/* <CatalogueProvider>
-        <CartProvider> */}
       <Provider>
         <ProductList type="catalogue" />
         <ProductList type="cart" />
       </Provider>
-      {/* </CartProvider>
-      </CatalogueProvider> */}
     </div>
   );
 }
