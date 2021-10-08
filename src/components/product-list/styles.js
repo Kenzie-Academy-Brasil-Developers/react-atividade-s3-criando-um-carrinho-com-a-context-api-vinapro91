363ae0b55fddbdaba9cee65e2a7c4ae2ff285f9e
vinicius <vinapro91@gmail.com>
2021-10-08 14:44:54 -0300

    fix: resolved padding of ul

--- src/components/product-list/styles.js
+++ src/components/product-list/styles.js
@@ -1,23 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 95vw;
+  width: 100%;
   height: 50vh;
 `;
 
 export const List = styled.ul`
   display: flex;
-  margin-top: 15px;
+  margin: 15px 0px 0px 0px;
   align-items: center;
   justify-content: center;
+  padding: 5px;
 
   li {
     display: flex;
     flex-direction: column;
-    margin: 5px;
+    margin: 0;
+    padding: 5px;
     list-style: none;
     border: 1px solid black;
-    width: 200px;
+    width: 150px;
     height: 150px;
     justify-content: center;
   }
